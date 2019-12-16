c31d4b83cb41d5a21f4d54d1a2f980c06b330dd1
nawatend <32384185+nawatend@users.noreply.github.com>
2019-12-16 09:22:17 +0100

    react mood cam init object detections working

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ObjectDetection from './components/ObjectDetection'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from 3 NMD </h1>
+      <h3>Run while you can.</h3>
+      <ObjectDetection/>
+      <h3>Anna DeLaaaaaaaaanghe & Nawang 10dar</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ObjectDetection.jsx
@@ -0,0 +1,120 @@
+import React, { Component } from 'react';
+import * as cocoSsd from '@tensorflow-models/coco-ssd';
+import '../App.css';
+
+
+class ObjectDetection extends Component {
+
+    state = {
+        width: 640,
+        height: 480
+    }
+
+    videoRef = React.createRef()
+    canvasRef = React.createRef()
+
+    detectObjectFromVideo = (model, video) => {
+        model.detect(video).then(detections => {
+            this.renderDetections(detections);
+
+            requestAnimationFrame(() => {
+                this.detectObjectFromVideo(model, video);
+            });
+        }, (error) => {
+            console.log("Couldn't start the webcam")
+            console.error(error)
+        });
+    };
+
+
+    renderDetections = detections => {
+        const ctx = this.canvasRef.current.getContext("2d");
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        const font = "18px Laca";
+        ctx.font = font;
+        ctx.textBaseline = "top";
+
+        detections.forEach(detection => {
+
+            if (detection.score.toFixed(2) >= 0.60) {
+                const x = detection.bbox[0];
+                const y = detection.bbox[1];
+                const width = detection.bbox[2];
+                const height = detection.bbox[3];
+                // Draw the bounding box.
+                ctx.strokeStyle = "#ffd369";
+                ctx.lineWidth = 1;
+                ctx.strokeRect(x, y, width, height);
+                // Draw the label background.
+                ctx.fillStyle = "#ffd369";
+                const textWidth = ctx.measureText(detection.class).width;
+                const textHeight = parseInt(font, 10);
+                // draw top left rectangle
+                ctx.fillRect(x, y, textWidth + 5, textHeight + 5);
+                // draw bottom left rectangle
+                ctx.fillRect(x, y + height - textHeight, textWidth, textHeight);
+
+                // Draw the text last to ensure it's on top.
+                ctx.fillStyle = "#015668";
+                ctx.fillText(detection.class, x, y);
+                ctx.fillText(detection.score.toFixed(2), x, y + height - textHeight);
+            }
+        });
+    };
+
+    componentDidMount() {
+        if (navigator.mediaDevices.getUserMedia || navigator.mediaDevices.webkitGetUserMedia) {
+            // define a Promise that'll be used to load the webcam and read its frames
+            const webcamPromise = navigator.mediaDevices
+                .getUserMedia({
+                    video: true,
+                    audio: false,
+                })
+                .then(stream => {
+                    // pass the current frame to the window.stream
+                    window.stream = stream;
+                    // pass the stream to the videoRef
+                    this.videoRef.current.srcObject = stream;
+
+                    return new Promise(resolve => {
+                        this.videoRef.current.onloadedmetadata = () => {
+                            resolve();
+                        };
+                    });
+                }, (error) => {
+                    console.log("Couldn't start the webcam")
+                    console.error(error)
+                });
+
+            // define a Promise that'll be used to load the model
+            const loadlModelPromise = cocoSsd.load();
+
+            // resolve all the Promises
+            Promise.all([loadlModelPromise, webcamPromise])
+                .then(values => {
+                    this.detectObjectFromVideo(values[0], this.videoRef.current);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+    }
+
+    render() {
+        return (
+            <div className="feed">
+                <video
+                    className="video"
+                    ref={this.videoRef}
+                    width="auto"
+                    height="auto"
+                    autoPlay
+                    muted
+                />
+                <canvas className="canvas" ref={this.canvasRef} width={this.state.width} height={this.state.height} />
+            </div>
+        )
+    }
+}
+
+export default ObjectDetection;
